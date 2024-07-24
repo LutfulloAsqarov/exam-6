6bf3457ac5169fb442203780e53752b20384f18d
LutfulloAsqarov <lutfulloasqarov9@gmail.com>
2024-07-24 08:43:28 +0500

    update

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import "./products.scss";
+import { FaStar } from "react-icons/fa";
 
 const Products = ({ data }) => {
     return (
         <div className="products__card">
-            <div>
+            <div className="products__card__img">
                 <img src={data.images[0]} alt="img" />
             </div>
             <div className="products__card__info">
+                <div className="products__card__star">
+                    <FaStar />
+                    <FaStar />
+                    <FaStar />
+                    <FaStar />
+                    <FaStar />
+                </div>
                 <h3>{data.title}</h3>
+                <p>{data.price}$</p>
             </div>
         </div>
     );
--- src/pages/home/arrival/Arrival.jsx
+++ src/pages/home/arrival/Arrival.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { useGetProductsQuery } from "../../../context/api/productApi";
 import Products from "../../../components/products/Products";
+import "./arrival.scss";
 
 const Arrival = () => {
     let { data } = useGetProductsQuery();
     return (
-        <section id="arrival">
+        <section id="products">
             <div className="container">
-                <div className="arrival">
-                    <div className="arrival__cards">
+                <div className="products">
+                    <div className="products__cards">
                         {data?.slice(0, 10).map((el) => (
                             <Products data={el} key={el.id} />
                         ))}
