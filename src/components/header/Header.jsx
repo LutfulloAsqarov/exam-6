24e572336b348a006dcdf345b9a6f44d023f1f34
LutfulloAsqarov <lutfulloasqarov9@gmail.com>
2024-07-28 09:11:09 +0500

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,11 +9,18 @@ import { CiHeart } from "react-icons/ci";
 import "./header.scss";
 import { IoClose } from "react-icons/io5";
 import { FaBars } from "react-icons/fa6";
+import { useGetValue } from "../../hooks/useGetValue";
+import { useGetProductsQuery } from "../../context/api/productApi";
 
 const Header = () => {
     let { pathname } = useLocation();
     const [showInput, setShowInput] = useState(false);
     const [toggle, setToggle] = useState(false);
+    const [searchValue, setSearchValue] = useState("");
+    const { data, isError } = useGetProductsQuery({
+        search: searchValue.trim(),
+    });
+    // const { formData, handleChange } = useGetValue();
     if (pathname.includes("register") || pathname.includes("admin")) {
         return <></>;
     }
@@ -122,7 +129,37 @@ const Header = () => {
                                 action=""
                                 className={`${showInput ? "show" : ""}`}
                             >
-                                <input type="text" placeholder="search..." />
+                                <input
+                                    type="text"
+                                    placeholder="search..."
+                                    name="search"
+                                    value={searchValue}
+                                    onChange={(e) =>
+                                        setSearchValue(e.target.value)
+                                    }
+                                />
+                                {searchValue ? (
+                                    <div className="header__search">
+                                        {!isError
+                                            ? data?.map((el) => (
+                                                  <Link
+                                                      to={`products/${el.id}`}
+                                                      key={el.id}
+                                                      className="header__search__items"
+                                                  >
+                                                      <img
+                                                          src={el.images[0]}
+                                                          alt=""
+                                                          width={50}
+                                                      />
+                                                      <p>{el.title}</p>
+                                                  </Link>
+                                              ))
+                                            : "No data"}
+                                    </div>
+                                ) : (
+                                    <></>
+                                )}
                             </form>
                             <ul>
                                 <li
--- src/pages/home/arrival/Arrival.jsx
+++ src/pages/home/arrival/Arrival.jsx
@@ -1,14 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { useGetProductsQuery } from "../../../context/api/productApi";
 import Products from "../../../components/products/Products";
 import "./arrival.scss";
+import { useGetCategoriesQuery } from "../../../context/api/categoryApi";
 
 const Arrival = () => {
-    let { data } = useGetProductsQuery({ page: 1, limit: 10 });
+    const [categoryValue, setCategoryValue] = useState("all");
+    let { data } = useGetProductsQuery({
+        page: 1,
+        limit: 10,
+        category: categoryValue === "all" ? "" : categoryValue,
+    });
+    const { data: categoryData } = useGetCategoriesQuery();
     return (
         <section id="products">
             <div className="container">
                 <div className="products">
+                    <ul className="category__list">
+                        <li className="category__item">
+                            <data
+                                onClick={(e) => {
+                                    setCategoryValue(e.target.value);
+                                    setPage(1);
+                                }}
+                                className={`category__data ${
+                                    categoryValue === "all"
+                                        ? "category__data-active"
+                                        : ""
+                                }`}
+                                value="all"
+                            >
+                                All
+                            </data>
+                        </li>
+                        {categoryData?.map((item) => (
+                            <li key={item.id} className="category__item">
+                                <data
+                                    onClick={(e) => {
+                                        setCategoryValue(e.target.value);
+                                        setPage(1);
+                                    }}
+                                    className={`category__data ${
+                                        categoryValue === item.title
+                                            ? "category__data-active"
+                                            : ""
+                                    }`}
+                                    value={item.title}
+                                >
+                                    {item.title}
+                                </data>
+                            </li>
+                        ))}
+                    </ul>
                     <div className="products__cards">
                         {data?.map((el) => (
                             <Products data={el} key={el.id} />
